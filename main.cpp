20433ae4116e70ce3dcc6115fa558a01292776bc
Deliverooo <ooconnor50@gmail.com>
2025-06-22 15:57:14 +0100

    c

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-void processInput(GLFWwindow *window);
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+#include <glm/glm.hpp>
 
 #define constexpr unsigned int WIDTH = 640;
 constexpr unsigned int HEIGHT = 480;
@@ -44,9 +40,6 @@ int main()
     // make the window's context current
     glfwMakeContextCurrent(window);
 
-    //use if you want to disable the mouse cursor e.g. for a first person camera
-    // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
     //enables v-sync
     glfwSwapInterval(1);
 
@@ -61,7 +54,6 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         currentFrame = glfwGetTime();
-        processInput(window);
 
         // clear all relevant buffers
         glClearColor(0.0f, 0.31f, 0.68f, 1.0f);
@@ -80,32 +72,4 @@ int main()
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
-}
-
-void processInput(GLFWwindow *window) {
-
-    // here you can add camera controls and other input stuff
-
-    //escape key closed window
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-
-}
-//the mouse callback
-//this will get called every time the mouse is moved
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
-
-    if (firstMouse) {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    double mouseDx = xpos - lastX;
-    double mouseDy = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
 }
